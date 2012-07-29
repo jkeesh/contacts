3df4bfb5b12a550ec7484a34de4799ef10509d1e
Jeremy Keeshin <jkeesh@gmail.com>
2012-07-29 00:00:11 -0700

    try updating wsgi config for settings"

--- wsgi.py
+++ wsgi.py
@@ -21,7 +21,7 @@ sys.path.append('/home/jkeesh/sites/contacts/')
 # This way print statements don't break our code
 sys.stdout = sys.stderr
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "contacts.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
