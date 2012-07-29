2c565fbd13a49c5dee34e9f017afb518b0888e10
Jeremy Keeshin <jkeesh@gmail.com>
2012-07-29 01:32:46 -0700

    add new people at today

--- todo/models.py
+++ todo/models.py
@@ -24,7 +24,7 @@ class Contact(models.Model):
 	name			=		models.CharField(max_length=100)	# The contacts name
 
 	# Next time to check in, default is one week from today
-	date			=		models.DateField(default=datetime.date.today() + timedelta(weeks=1))					
+	date			=		models.DateField(default=datetime.date.today())					
 
 
 class Note(models.Model):
